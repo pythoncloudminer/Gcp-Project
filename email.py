585706a3469c48b6521e14a0c4c04f1f76635e39
pythoncloudminer <141788762+pythoncloudminer@users.noreply.github.com>
2024-01-09 02:20:26 +0530

    emai body updated - email.py
    Modified the template for body of the email

--- email.py
+++ email.py
@@ -4,6 +4,7 @@ from airflow.operators.python_operator import PythonOperator
 from airflow.operators.bash_operator import BashOperator
 from google.cloud import bigquery
 import smtplib
+from email.message import EmailMessage
 
 
 default_args = {
@@ -30,27 +31,39 @@ def bigquery_email_list():
     print("Need to work on it")
 
 def get_email_dict(**kwargs):
+    ti = kwargs['ti']
     email_dict = dict()
     x = [['task_1', 'sudharshanvas@gmail.com'], ['task_2', 'sudharshanvas@gmail.com', 'sudharshanvas@gmail.com','sudharshanvas@gmail.com'],
     ['task_3', 'sudharshanvas@gmail.com', 'sudharshanvas@gmail.com', 'sudharshanvas@gmail.com']]
     for i in x:
         email_dict[str(i[0])] = i[1:]
+    ti.xcom_push("email", email_dict)
     return email_dict
 
 def send_email(**kwargs):
+    email = EmailMessage()
+    email["From"] = "sudharshangcp@gmail.com"
+    #email["To"] = recipient
+    email["Subject"] = "Certificate Expiry Notification"
+    #email.set_content(message, subtype="html")
+
     ti = kwargs['ti']
-    password = "<app_password>"
+    password = "upzkqbwxnmwfcptw "
     s = smtplib.SMTP('smtp.gmail.com', 587)
     s.starttls()
     s.login('sudharshangcp@gmail.com',password)
     email_dict = ti.xcom_pull(task_ids = "format_email")
     for i in email_dict.items():
         key = i[0]
-        message = f"Hi Hope you are doing well \n"\
-        f"This is a test email from {key}"
+        message = f"""<h1> Your Certificate is about to Expire in seven days! </h1>
+        <p>Hi Hope you are doing <b>well</b><p> \n
+        "This is a test email from {key} to notify the expiry of your certificate"""
         vals = i[1]
         for mail in vals:
-            s.sendmail('sudharshangcp@gmail.com',mail,message)
+            email.set_content(message, subtype="html")
+            email["To"] = mail
+            s.sendmail('sudharshangcp@gmail.com',mail,email.as_string())
+            del email["To"]
 
 
 
